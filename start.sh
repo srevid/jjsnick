899d7daf72526e16da2db3202019e234ca204299
snickk <s_lessnick@yshoo.com>
2016-01-10 17:00:03 -0500

    worldsave

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd minecraft
+java \
+  -d64 \
+  -server \
+  -Xms1G -Xmx1G \
+  -XX:+UseConcMarkSweepGC \
+  -XX:+UseParNewGC \
+  -XX:+CMSIncrementalPacing \
+  -XX:ParallelGCThreads=2 \
+  -XX:+AggressiveOpts \
+  -jar spigot-1.8.8.jar nogui
+
