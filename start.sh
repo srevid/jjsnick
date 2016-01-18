5c431a1942c0cd5f768e9a42f057ab0cdd1d1682
snickk <s_lessnick@yshoo.com>
2016-01-17 23:38:13 -0500

    worldsave

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-java \
-  -d64 \
-  -server \
-  -Xms128M -Xmx1G \
-  -XX:+UseConcMarkSweepGC \
-  -XX:+UseParNewGC \
-  -XX:+CMSIncrementalPacing \
-  -XX:ParallelGCThreads=2 \
-  -XX:+AggressiveOpts \
-  -jar spigot-1.8.8.jar nogui
+echo
+echo Minecraft is now started via supervisor
+echo http://supervisord.org/
+echo Config file is symlinked from minecraft/supervisor.conf
+echo
+echo  - snickk
+echo
